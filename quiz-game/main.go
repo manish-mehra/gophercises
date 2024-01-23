2fa82225fdff5faad1ba89a0719b7c392046b991
manish-mehra <manishmehra1469@gmail.com>
2024-01-23 15:38:46 +0530

    refactor code

--- quiz-game/main.go
+++ quiz-game/main.go
@@ -16,39 +16,51 @@ func check(e error, warning string) {
 	}
 }
 
+func openCSVFile(fileName string) *os.File {
+	file, err := os.Open(fileName)
+	check(err, "Error opening CSV file")
+	return file
+}
+
+func readCSV(file *os.File) [][]string {
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	check(err, "Error reading CSV file")
+	return records
+}
+
+func getUserInput() string {
+	inputReader := bufio.NewReader(os.Stdin)
+	userInput, err := inputReader.ReadString('\n')
+	check(err, "Error reading input")
+	return strings.TrimSpace(userInput)
+}
+
+func processQuestions(records [][]string) int {
+	totalScore := 0
+
+	for _, record := range records {
+		question := record[0]
+		answer := record[1]
+		fmt.Println("question: ", question)
+		userInput := getUserInput()
+		if userInput == answer {
+			totalScore++
+		}
+	}
+	return totalScore
+}
+
 func main() {
 
 	fileName := flag.String("fName", "problems_1.csv", "a file")
 	flag.Parse()
 
-	file, err := os.Open(*fileName)
-	check(err, "Error opening csv file")
+	file := openCSVFile(*fileName)
 	defer file.Close()
 
-	// create csv reader
-	reader := csv.NewReader(file)
+	records := readCSV(file)
 
-	records, rError := reader.ReadAll()
-	check(rError, "Error reading csv")
-
-	totalScore := 0
-
-	inputReader := bufio.NewReader(os.Stdin)
-
-	for _, record := range records {
-
-		question := record[0]
-		answer := record[1]
-		fmt.Println("question: ", question)
-		userInput, err := inputReader.ReadString('\n')
-		check(err, "Error reading input")
-
-		userInput = strings.TrimSpace(userInput)
-
-		if userInput == answer {
-			totalScore++
-		}
-	}
-
+	totalScore := processQuestions(records)
 	fmt.Printf("%d out of %d correct", totalScore, len(records))
 }
