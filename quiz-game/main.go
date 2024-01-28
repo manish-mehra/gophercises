0023b9455fd8afde972ec139ae7eebbb112ed3b7
manish-mehra <manishmehra1469@gmail.com>
2024-01-28 20:45:51 +0530

    finished part 2 & bonus: timed quiz & shuffle quiz with flag

--- quiz-game/main.go
+++ quiz-game/main.go
@@ -1,18 +1,19 @@
 package main
 
 import (
-	"bufio"
 	"encoding/csv"
 	"flag"
 	"fmt"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 func check(e error, warning string) {
 	if e != nil {
 		fmt.Println(warning, e)
-		return
+		os.Exit(1)
 	}
 }
 
@@ -29,38 +30,83 @@ func readCSV(file *os.File) [][]string {
 	return records
 }
 
-func getUserInput() string {
-	inputReader := bufio.NewReader(os.Stdin)
-	userInput, err := inputReader.ReadString('\n')
-	check(err, "Error reading input")
-	return strings.TrimSpace(userInput)
-}
+func shuffleQuiz(problems []problem) []problem {
 
-func processQuestions(records [][]string) int {
-	totalScore := 0
+	source := rand.NewSource(time.Now().UnixNano())
+	random := rand.New(source)
 
-	for _, record := range records {
-		question := record[0]
-		answer := record[1]
-		fmt.Println("question: ", question)
-		userInput := getUserInput()
-		if userInput == answer {
-			totalScore++
-		}
+	min := 0
+	max := len(problems) - 1
+
+	var temp problem
+
+	for i := 0; i < len(problems); i++ {
+		randomIndex := random.Intn(max-min+1) + min
+		temp = problems[i]
+		problems[i] = problems[randomIndex]
+		problems[randomIndex] = temp
 	}
-	return totalScore
+
+	return problems
 }
 
 func main() {
 
 	fileName := flag.String("fName", "problems_1.csv", "a file")
+	timeLimit := flag.Int("limit", 3, "time limit in seconds")
+	shuffle := flag.Bool("shuffle", false, "flag to shuffle quiz")
 	flag.Parse()
 
 	file := openCSVFile(*fileName)
 	defer file.Close()
 
 	records := readCSV(file)
+	problems := parseLines(records)
 
-	totalScore := processQuestions(records)
-	fmt.Printf("%d out of %d correct", totalScore, len(records))
+	if *shuffle {
+		fmt.Println("suffled")
+		problems = shuffleQuiz(problems)
+	}
+
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+	correct := 0
+
+problemloop:
+	for index, problem := range problems {
+		fmt.Printf("problem #%d: %s = ", index+1, problem.question)
+		answerCh := make(chan string)
+
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Println()
+			break problemloop
+		case answer := <-answerCh:
+			if answer == problem.answer {
+				correct++
+			}
+		}
+	}
+	fmt.Printf("You scored %d out of %d. \n", correct, len(records))
+}
+
+func parseLines(lines [][]string) []problem {
+	problems := make([]problem, len(lines))
+	for index, line := range lines {
+		problems[index] = problem{
+			question: line[0],
+			answer:   strings.TrimSpace(line[1]),
+		}
+	}
+	return problems
+}
+
+type problem struct {
+	question string
+	answer   string
 }
