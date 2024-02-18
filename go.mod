2ca016b877eac2da1ea072f0c09af31a70584a2c
manish-mehra <manishmehra1469@gmail.com>
2024-02-18 23:09:51 +0530

    init

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/manish-mehra/gophercises
 
 go 1.21.5
 
-require gopkg.in/yaml.v3 v3.0.1 // indirect
+require (
+	golang.org/x/net v0.21.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- html-link-parser/main.go
+++ html-link-parser/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
 func main() {
+
+	file, err := os.ReadFile("./ex2.html")
+	if err != nil {
+		panic(err)
+	}
+
+	htmlString := string(file)
+
+	r := strings.NewReader(htmlString)
+	links, err := Parse(r)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", links)
 
 }
